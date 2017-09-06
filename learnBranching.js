e69e080a2dcfd2ed8cae1f81bc2d9010c4425fd2
Divyanshu Singh <dsinecos@gmail.com>
2017-09-06 10:53:39 +0530

    Third commit - Authentication branch

--- learnBranching.js
+++ learnBranching.js
@@ -2,3 +2,4 @@ console.log("First commit on master branch");
 
 console.log("First commit on authentication branch");
 console.log("Second commit on authentication branch");
+console.log("Third commit on authentication branch");
