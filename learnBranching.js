18ab367bd699c61c2a7c0d15020489e642d28d7d
Divyanshu Singh <dsinecos@gmail.com>
2017-09-06 12:13:31 +0530

    Eighth commit - Master branch

--- learnBranching.js
+++ learnBranching.js
@@ -1,4 +1,5 @@
 console.log("Seventh commit on master branch");
+console.log("Eighth commit on master branch");
 
 console.log("First commit on authentication branch");
 console.log("Second commit on authentication branch");
@@ -6,11 +7,7 @@ console.log("Third commit on authentication branch");
 console.log("Fourth commit on authentication branch");
 console.log("Fifth commit on authentication branch");
 
-<<<<<<< HEAD
-console.log("First commit on oauth branch");
-=======
 console.log("First commit on oauth branch");
 console.log("Second commit on oauth branch");
 console.log("Third commit on oauth branch");
 console.log("Fourth commit on oauth branch");
->>>>>>> oauth
