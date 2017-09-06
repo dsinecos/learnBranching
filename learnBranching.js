22effcff92a4356ac6d2ba1f1d79589a3ce34514
Divyanshu Singh <dsinecos@gmail.com>
2017-09-06 10:57:37 +0530

    Third commit - Master branch

--- learnBranching.js
+++ learnBranching.js
@@ -1,5 +1,6 @@
 console.log("First commit on master branch");
 console.log("Second commit on master branch");
+console.log("Third commit on master branch");
 
 console.log("First commit on authentication branch");
 console.log("Second commit on authentication branch");
